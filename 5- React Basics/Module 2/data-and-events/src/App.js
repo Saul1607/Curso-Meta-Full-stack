467d1bb211a1368d86b4ab93b417644311689e7a
Saúl Solís <saulsolissierra07@gmail.com>
2025-07-17 14:48:07 -0600

    Ejemplo de useState

--- 5- React Basics/Module 2/data-and-events/src/App.js
+++ 5- React Basics/Module 2/data-and-events/src/App.js
@@ -1,12 +1,19 @@
 import Child from "./Child"
+import Heading from "./Heading";
 import './App.css';
+import { useState } from "react";
 
 function App() {
-  const date = new Date();
+  const [word, setWord] = useState('Eat');
 
+  function handleClick() {
+    setWord('Drink');
+  }
+  
   return (
-    <div>
-      <Child message={date.toLocaleTimeString()} />
+    <div className="App">
+      <Heading message={word + " at Little Lemon"} />
+      <button onClick={handleClick}>Click here</button>
     </div>
   );
 }
