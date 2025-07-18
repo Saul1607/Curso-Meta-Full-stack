1ac57b3330f58553c07520d1d08b24930a8339fc
Saúl Solís <saulsolissierra07@gmail.com>
2025-07-18 17:12:00 -0600

    Videos con React Player

--- 5- React Basics/Module 3/using-assets/src/App.js
+++ 5- React Basics/Module 3/using-assets/src/App.js
@@ -1,28 +1,34 @@
-import rooftops from './assets/images/central-park.jpg'
+import rooftops from './assets/images/central-park.jpg';
+import ReactPlayer from 'react-player';
 import './App.css';
 
 function App() {
   const randomImageUrl = "https://picsum.photos/400/265"
-
   return (
     <div>
+      <ReactPlayer 
+        src='https://www.youtube.com/watch?v=dQw4w9WgXcQ' 
+        controls={true}
+        playing={true}
+        volume={0.5}
+      />
       <h1>
         Task: add thre images with some styling
       </h1>
       <img 
         height={200} 
         src={rooftops} 
-        alt='An image of a rooftop in New York'
+        alt='A rooftop in New York'
       />
       <img 
         height={200}
         src={require("./assets/images/central-park.jpg")}
-        alt='An image of a rooftop in New York'
+        alt='A rooftop in New York'
       />
       <img 
         height={200}
         src={randomImageUrl}
-        alt='Random Image'
+        alt='Random'
       />
     </div>
   );
