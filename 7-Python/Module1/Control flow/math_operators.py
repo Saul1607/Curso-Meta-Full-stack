3b75def02d851e0abfca488228ce5a62f1dfb3af
Saúl Solís <saulsolissierra07@gmail.com>
2025-07-31 23:08:17 -0600

    Ejemeplos operadores python

---
+++ 7-Python/Module1/Control flow/math_operators.py
@@ -0,0 +1,17 @@
+#Math operators
+print(2 + 2) #4
+print(2 - 2) #0
+print(35 / 5) #7 retorna float en vez de int
+print(35 * 5) #175
+
+#Logical operators
+a, b = True, False
+
+if a and b:
+    print("All true!")
+
+if a or b:
+    print("At least one true!")
+
+if not(b) or not(a):
+    print("At least one of the values was false")
